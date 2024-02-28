92074e99c2b01ce43e1cdfd93e81a0637f82280c
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-02-28 09:33:25 +0530

    PC fromConfig Update

--- resources/views/productions/subheader.blade.php
+++ resources/views/productions/subheader.blade.php
@@ -5,9 +5,9 @@
                 <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Client Information</h5>
             </div>
         </div>
-        <div class="d-flex align-items-start">
+        {{-- <div class="d-flex align-items-start">
             <a class="btn btn-light-primary font-weight-bolder btn-sm mr-5"
                 href="{{ url('clients') }}?parent={{ request()->parent }}&child={{ request()->child }}">List</a>
-        </div>
+        </div> --}}
     </div>
 @endsection
