5dff6290a0afa4cdf927cb30464a55585698d789
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-03-18 10:11:36 +0530

    PC dynamic client sub project integration

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -570,7 +570,7 @@ class ProductionController extends Controller
                 $data = [];
                 foreach ($request->except('_token', 'parent', 'child') as $key => $value) {
                     if (is_array($value)) {
-                        $data[$key] = implode(',', $value);
+                        $data[$key] = implode('_', $value);
                     } else {
                         $data[$key] = $value;
                     }
