afc939e080a308156e765586225806cb266aef4a
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-04-08 11:58:12 +0530

    pc logo change in login and all pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<?php
-$currentURL = URL::current();
-$routex = explode('/', Route::current()->uri());
-$current_page = Route::getFacadeRoot()->current()->uri();
-$url_segment = Request::segment(1);
-?>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,8 +8,7 @@ $url_segment = Request::segment(1);
     @include('layouts/header_script')
     <style>
         /* Additional styling for the page */
-        body,
-        html {
+        body, html {
             margin: 0;
             padding: 0;
             height: 100%;
@@ -24,41 +16,31 @@ $url_segment = Request::segment(1);
 
         .login-container {
             display: flex;
-            min-height: 100%;
+            min-height: 100vh;
             background-color: #ffffff;
-            background-image: url("assets/media/bg/login_background_3.svg");
+            /* background-image: url("assets/media/bg/login_background_3.svg"); */
             background-size: cover;
             background-position: bottom !important;
             color: white;
             position: relative;
         }
 
-        .left-side,
-        .right-side {
-            display: flex;
-            flex-direction: column;
+        .left-side, .right-side {
+            flex: 1;
         }
 
         .left-side {
-            flex: 0.6; /* Occupy 60% of the screen width */
-            justify-content: center;
-            align-items: center;
-        }
-
-        .left-side img {
-            width: 100%;
-            max-width: 200px;
-            height: auto;
-            margin-bottom: 20px;
+            background-image: url('{{ asset("assets/media/bg/login_background_4.svg") }}');
+            background-size: cover;
+            /* background-position: center; */
+            background-repeat: no-repeat;
         }
 
         .right-side {
-            flex: 0.4; /* Occupy 40% of the screen width */
-            position: relative;
-            padding: 20px;
             display: flex;
             justify-content: center;
             align-items: center;
+            padding: 20px;
         }
 
         .login-card {
@@ -68,20 +50,20 @@ $url_segment = Request::segment(1);
             border-radius: 10px;
             padding: 20px;
             box-shadow: 0 4px 6px rgba(226, 216, 216, 0.1);
-            margin-bottom: 0px;
             color: #191C24;
         }
 
-        .login-card form {
-            margin-bottom: 0;
+        @media (max-width: 768px) {
+            .login-container {
+                flex-direction: column;
+            }
         }
-
         .copyright-container {
             position: absolute;
             bottom: 10px;
-            left: 23.5rem;
+            left: 22.5rem;
             transform: translateX(-50%);
-            font-size: 12px;
+            font-size: 11px;
             display: flex;
             justify-content: space-between;
             align-items: center;
@@ -100,31 +82,17 @@ $url_segment = Request::segment(1);
             color: #191C24;
             font-weight: 500;
         }
-
-        @media (max-width: 767px) {
-            .login-container {
-                flex-direction: column;
-            }
-            .left-side,
-            .right-side {
-                flex: 1; /* Occupy full width on smaller screens */
-            }
-        }
     </style>
 </head>
-
 <body>
-
     <div class="login-container">
-        <div class="left-side">
-            {{-- Content for the left side (e.g., logo) --}}
-        </div>
+        <div class="left-side"></div>
         <div class="right-side">
             <div class="login-card">
-                <div class="text-left  mt-0 mb-12 mb-lg-12 ml-8 mr-8">
+                <div class="text-left mt-0 mb-12 mb-lg-12 ml-8 mr-8">
                     <h3 class="font-size-h1">Sign In</h3>
                 </div>
-                @yield('content') {{-- Your login form content --}}
+                @yield('content')
             </div>
         </div>
     </div>
@@ -135,5 +103,4 @@ $url_segment = Request::segment(1);
 </body>
 @include('layouts/footer_script_login')
 @include('layouts/flashMessage')
-
 </html>
