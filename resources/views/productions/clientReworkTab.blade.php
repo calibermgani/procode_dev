6b7286cb72fc2053ab1c2ffcda64568d5b6e3f98
ashique-caliber <mashique@caliberfocus.com>
2024-04-22 14:15:05 +0530

    Assigned to rework assihque

--- resources/views/productions/clientReworkTab.blade.php
+++ resources/views/productions/clientReworkTab.blade.php
@@ -38,7 +38,7 @@
                             <div class="wizard-wrapper py-2">
                                 <div class="wizard-label p-2 mt-2">
                                     <div class="wizard-title" style="display: flex; align-items: center;">
-                                        <h6 style="margin-right: 5px;">Assigned</h6>
+                                        <h6 style="margin-right: 5px;">Total Inventory</h6>
                                         @include('CountVar.countRectangle', ['count' => $assignedCount])
 
                                     </div>
@@ -79,7 +79,7 @@
                             <div class="wizard-wrapper py-2">
                                 <div class="wizard-label p-2 mt-2">
                                     <div class="wizard-title" style="display: flex; align-items: center;">
-                                        <h6 style="margin-right: 5px;">Rework</h6>
+                                        <h6 style="margin-right: 5px;">Audit Rework</h6>
                                         @include('CountVar.countRectangle', ['count' => $reworkCount])
                                     </div>
                                 </div>
