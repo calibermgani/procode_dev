d2a5fc12e62e74b6fd708f838237f6a771439038
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-04-30 15:51:59 +0530

    unassigned tab in coder production

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -199,8 +199,9 @@
                                                                         title="View"><i
                                                                         class="fa far fa-eye text-eye icon-circle1 mt-0"></i></button>
                                                             </td>
-                                                            @foreach ($data->getAttributes() as $columnName => $columnValue)
+                                                            @foreach ($columnsHeader as $columnName => $header)
                                                                 @php
+                                                                 $columnValue = isset($data->{$header}) && !empty($data->{$header}) ? $data->{$header} : "--";
                                                                     $columnsToExclude = [
                                                                         'QA_emp_id',
                                                                         'ce_hold_reason','qa_hold_reason','qa_work_status','QA_required_sampling','QA_rework_comments','coder_rework_status','coder_rework_reason','coder_error_count','qa_error_count','tl_error_count','tl_comments','QA_status_code','QA_sub_status_code','QA_followup_date','CE_status_code','CE_sub_status_code','CE_followup_date',
