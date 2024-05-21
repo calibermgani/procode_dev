a01625e04c4d589f1f539ec94a8e411ab67eb808
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-05-21 17:01:43 +0530

    dashboard projects

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,11 @@
 <?php
-// if(strcmp($_SERVER['HTTP_HOST'],'35.208.83.145') == 0){
-//     return [
-//          'PRO_CODE_URL' => 'https://aims.officeos.in',
-//     ];
-// } else {
+if(strcmp($_SERVER['HTTP_HOST'],'35.208.83.145') == 0){
+    return [
+         'PRO_CODE_URL' => 'https://aims.officeos.in',
+    ];
+} else {
     return [
          'PRO_CODE_URL' => 'http://dev.aims.officeos.in',
     ];
-// }
+    
+}
