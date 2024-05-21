8084cf7d72dfdd6b62f1f1698272a3183ad7ab84
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-05-21 15:14:47 +0530

    constant file commit

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,10 @@
 <?php
-
-return [
-    'PRO_CODE_URL' => 'https://aims.officeos.in/',
-];
+if(strcmp($_SERVER['HTTP_HOST'],'35.208.83.145') == 0){
+    return [
+         'PRO_CODE_URL' => 'https://aims.officeos.in',
+    ];
+} else {
+    return [
+         'PRO_CODE_URL' => 'http://dev.aims.officeos.in',
+    ];
+}
