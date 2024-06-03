25c5fbc87ec0b0035b8ae7221a691d7f4ce531a3
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-06-03 17:35:05 +0530

    sop list to live

--- resources/views/productions/clientCompletedTab.blade.php
+++ resources/views/productions/clientCompletedTab.blade.php
@@ -5,7 +5,7 @@
                     <div class="card-body p-0">
                         @php
                              $empDesignation = Session::get('loginDetails') &&  Session::get('loginDetails')['userDetail']['user_hrdetails'] &&  Session::get('loginDetails')['userDetail']['user_hrdetails']['current_designation']  !=null ? Session::get('loginDetails')['userDetail']['user_hrdetails']['current_designation']: "";
-                            //$encodedId = App\Http\Helper\Admin\Helpers::encodeAndDecodeID(Str::lower($databaseConnection));
+                             $loginEmpId = Session::get('loginDetails') &&  Session::get('loginDetails')['userDetail'] && Session::get('loginDetails')['userDetail']['emp_id'] !=null ? Session::get('loginDetails')['userDetail']['emp_id']:"";
                         @endphp
                         <div class="card-header border-0 px-4">
                             <div class="row">
