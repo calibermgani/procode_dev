c2316fca97d0c39e9e712bba878545853bb9c24f
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-06-03 16:19:52 +0530

    sop list to live

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -140,8 +140,8 @@ class SettingController extends Controller
                     $sopDisplayName = pathinfo($attachmentName, PATHINFO_FILENAME);
                     $onlyFileName = str_replace(' ', '_', $sopDisplayName);
                     $fileNames = $onlyFileName . '_' . date('YmdHis') . '.' . $extension6;
-
-                    if (!Storage::exists('public/pdf_folder/')) {dd('if');
+                    dd(Storage::exists('public/pdf_folder/'));
+                    if (!Storage::exists('public/pdf_folder/')) {
                         $storage_path = Storage::makeDirectory('/pdf_folder/', 0775, true);
                         $attachment->storeAs('public/pdf_folder/', $fileNames);
                     } else {
