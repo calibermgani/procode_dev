cd4fa03234eadc2291c95bda74a9e2ea84bcb35b
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-06-13 11:05:19 +0530

    constants file in dev

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,7 @@
 <?php
 
 $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+echo $_SERVER['HTTP_HOST'];
 dd($host);
 if (strcmp($host, '35.208.83.145') == 0 || strcmp($host, 'procode.officeos.in') == 0) {
     return [
