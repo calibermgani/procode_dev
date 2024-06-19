f382458fbe1a48f56be51654dbfe95e9b527fa5a
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-06-19 18:03:48 +0530

    constant file url issue

--- config/constants.php
+++ config/constants.php
@@ -2,20 +2,12 @@
 
 $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
 
-if (php_sapi_name() == 'cli') {
-    // Set default value for CLI context
-    $host = 'localhost';
-} else {
-    // In web context, use the actual HTTP_HOST
-    $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
-}
-echo 'Host: ' . $host . PHP_EOL;
-if ($host === '35.208.83.145' || $host === 'procode.officeos.in') {
+if (strcmp($host, '35.208.83.145') == 0 || strcmp($host, 'procode.officeos.in') == 0) {
     return [
         'PRO_CODE_URL' => 'https://aims.officeos.in',
     ];
 } else {
     return [
-        'PRO_CODE_URL' => 'http://dev.aims.officeos.in', // Change this to the appropriate URL
+        'PRO_CODE_URL' => 'https://aims.officeos.in',
     ];
 }
