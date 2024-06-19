753b7731cee956c0795271d7e4637c27c4537af5
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-06-19 17:54:04 +0530

    constant file url issue

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
-echo 'Host: ' . $_SERVER['HTTP_HOST'] . PHP_EOL;
+
 if (strcmp($host, '35.208.83.145') == 0 || strcmp($host, 'procode.officeos.in') == 0) {
     return [
         'PRO_CODE_URL' => 'https://aims.officeos.in',
