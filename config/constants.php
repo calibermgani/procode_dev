995d1ac50d785f994d75c1e72418c343a98f59fb
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-07-18 12:40:15 +0530

    toc denial

--- config/constants.php
+++ config/constants.php
@@ -5,17 +5,16 @@ if (php_sapi_name() == 'cli') {
     // For CLI context, set a default value for $host
     $host = 'localhost';
 } else {
-    // For web context, use the actual HTTP_HOST if available, otherwise default to 'localhost'
+    // For web context, use the actual HTTP_HOST if available, otherwise default to  'localhost'
     $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
 }
-// dd($host);
-// Use the value of $host to determine the PRO_CODE_URL
+
 if ($host === '35.208.83.145' || $host === 'procode.officeos.in') {
     return [
         'PRO_CODE_URL' => 'https://aims.officeos.in',
     ];
 } else {
     return [
-        'PRO_CODE_URL' => 'http://dev.aims.officeos.in',
+        'PRO_CODE_URL' => 'https://aims.officeos.in',
     ];
 }
