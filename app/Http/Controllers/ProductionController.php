243eebf014540605a1bb21f8c7460bbf157477f0
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-05 13:41:48 +0530

    play button issue

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -1083,20 +1083,32 @@ class ProductionController extends Controller
                     $modelClass::create($data);
                 }
                 $currentTime = Carbon::now();
-                $callChartWorkLogExistingRecord = CallerChartsWorkLogs::where('record_id', $data['parent_id'])
+                // $callChartWorkLogExistingRecord = CallerChartsWorkLogs::where('record_id', $data['parent_id'])
+                // ->where('record_status',$data['record_old_status'])
+                // ->where('project_id', $decodedProjectName)
+                // ->where('sub_project_id', $decodedPracticeName)
+                // ->where('emp_id', Session::get('loginDetails')['userDetail']['emp_id'])->where('end_time',NULL)->first();
+                //   if($callChartWorkLogExistingRecord && $callChartWorkLogExistingRecord != null) {
+                //     $start_time = Carbon::parse($callChartWorkLogExistingRecord->start_time);
+                //     $time_difference = $currentTime->diff($start_time);
+                //     $work_time = $currentTime->diff($start_time)->format('%H:%I:%S');
+                //     $callChartWorkLogExistingRecord->update([
+                //         'record_status' => $data['chart_status'],
+                //         'end_time' => $currentTime->format('Y-m-d H:i:s'),'work_time' => $work_time
+                //     ]);
+                // }
+                $callChartWorkLogExistingRecords = CallerChartsWorkLogs::where('record_id', $data['parent_id'])
                 ->where('record_status',$data['record_old_status'])
                 ->where('project_id', $decodedProjectName)
                 ->where('sub_project_id', $decodedPracticeName)
-                ->where('emp_id', Session::get('loginDetails')['userDetail']['emp_id'])->where('end_time',NULL)->first();
-                  if($callChartWorkLogExistingRecord && $callChartWorkLogExistingRecord != null) {
-                    $start_time = Carbon::parse($callChartWorkLogExistingRecord->start_time);
-                    $time_difference = $currentTime->diff($start_time);
-                    $work_time = $currentTime->diff($start_time)->format('%H:%I:%S');
-                    $callChartWorkLogExistingRecord->update([
-                        'record_status' => $data['chart_status'],
-                        'end_time' => $currentTime->format('Y-m-d H:i:s'),'work_time' => $work_time
-                    ]);
-                }
+                ->where('emp_id', Session::get('loginDetails')['userDetail']['emp_id'])->where('end_time',NULL)->get();
+                    if ($callChartWorkLogExistingRecords->isNotEmpty()) {
+                        foreach ($callChartWorkLogExistingRecords as $callChartWorkLog) {
+                            $start_time = Carbon::parse($callChartWorkLog->start_time);
+                            $work_time = $currentTime->diff($start_time)->format('%H:%I:%S');
+                            $callChartWorkLog->update( ['record_status' => $data['chart_status'],'end_time' => $currentTime->format('Y-m-d H:i:s'),'work_time' => $work_time] );
+                        }
+                   }
                 $tabUrl = $data['record_old_status'] == "Revoke" ? $data['record_old_status'] : lcfirst(str_replace('CE_', '', $data['record_old_status']));
                 return redirect('/projects_'.$tabUrl.'/'.$clientName.'/'.$subProjectName);
              } catch (\Exception $e) {
